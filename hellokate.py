f11a070a2c530b5316460a10b20b39ab585b6656
unknown <g00048625@gmit.ie>
2021-01-29 15:01:16 +0000

    week 02 labwork

---
+++ hellokate.py
@@ -0,0 +1,13 @@
+# practice name coding and running 
+# Author: Kate Leddy
+
+name= " Kate "
+print ('hello ' + name )
+
+# age 
+age = 35
+print ('your age is ' + str(age))
+
+# formatting 
+
+print ('your age is {} '.format (age))
---
+++ helloworld.py
@@ -0,0 +1,5 @@
+#hello world practice typing code
+# author Kate Leddy
+
+print ('hello world!')
+print ('hello world \nhow are you today')
