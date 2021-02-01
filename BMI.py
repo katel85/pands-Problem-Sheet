e0288b1ac4206f8e1d51f0e283815f87f8950db8
unknown <g00048625@gmit.ie>
2021-02-01 20:55:28 +0000

    week 2 labs

--- BMI.py
+++ BMI.py
@@ -1,6 +1,25 @@
 # BMI labwork 2.2 week 2 
-# Author Leddy
+# Author  Catherine Leddy
 
 name = input ('hey what is your name')
 print ( 'Hello' + name )
+
+height = float(input('Hey{} what is your height in cm :' .format (name)))
+weight = float(input('Hey{} name what is your weight in kg :' .format(name)))
+
+# need to put in a way so that we can put in a decimal point with the kg. Intially used 
+# int but because there is a decimal point I will use float
+
+Newheight = ((height/100)**2)
+
+BMI= float( weight / Newheight)
+print("Your BMI is {}".format (BMI))
+
+#initially my BMI was coming out at 55 because I had the height as an int. My answer was going to be with decimal points 
+# so this also had to be changed to float along with the weight 
+
+
+
+
+
 
