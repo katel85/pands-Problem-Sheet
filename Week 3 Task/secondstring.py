8d6918fd060188a4429f57748528f2e604b955c5
unknown <g00048625@gmit.ie>
2021-04-08 21:34:42 +0100

    Week03TaskUpdated

--- Week 3 Task/secondstring.py
+++ Week 3 Task/secondstring.py
@@ -1,21 +1,21 @@
 # Write a program that takes asks a user to input a string and outputs every second letter in reverse order.
 # Author Catherine Leddy
-# ref https://stackoverflow.com/questions/20847205/program-to-extract-every-alternate-letters-from-a-string-in-python
-# ref https://www.w3schools.com/python/python_howto_reverse_string.asp
-# ref 
 
 
 
-sent = 'The quick brown fox jumps over the lazy dog.'
+
+
+sent = str(input("Please enter a sentence and I will output ever second letter for you in reverse order!"))
 sent2 = (sent[::-1])
-
 print (sent2[::2])
+print ('Hope you like your new sentence!')
 
-# first task was to reverse the letters in the sentence and create a new sentence 
-# backwards with sent2 = (sent[::-1])
+# first task was to  create a string asking the user to input a sentence. This was defined as sent.
+# The sent was then coded to  read backwards with sent2 = (sent[::-1])
 # second task was to pick every second letter in the reversed sentence with print (sent2[::2])
 
-
+# ref https://stackoverflow.com/questions/20847205/program-to-extract-every-alternate-letters-from-a-string-in-python
+# ref https://www.w3schools.com/python/python_howto_reverse_string.asp
 
 
 
